0429c68ec308c015ca72b8a6765606bee21b3330
Alexander Garcia <alexandergv2117@gmail.com>
2023-05-19 17:51:16 -0500

    Initial commit

---
+++ Domain/Entities/Area.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    [Table("Areas")]
+    public class Area
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public virtual ICollection<Empleado> Empleados { get; set; }
+    }
+}
---
+++ Repository/Migrations/20230519213923_19-05-2023.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class _19052023 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Correo",
+                table: "Personas",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Personas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "IdArea",
+                table: "Personas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumEmpleado",
+                table: "Personas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Areas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Areas", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personas_IdArea",
+                table: "Personas",
+                column: "IdArea");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Personas_Areas_IdArea",
+                table: "Personas",
+                column: "IdArea",
+                principalTable: "Areas",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Personas_Areas_IdArea",
+                table: "Personas");
+
+            migrationBuilder.DropTable(
+                name: "Areas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Personas_IdArea",
+                table: "Personas");
+
+            migrationBuilder.DropColumn(
+                name: "Correo",
+                table: "Personas");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Personas");
+
+            migrationBuilder.DropColumn(
+                name: "IdArea",
+                table: "Personas");
+
+            migrationBuilder.DropColumn(
+                name: "NumEmpleado",
+                table: "Personas");
+        }
+    }
+}
